f52b393cf6af2df0e4986fd69997bc521b7faa8c
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-06 10:20:07 +0530

    Loaded proper jquery

--- resources/views/read-bible.blade.php
+++ resources/views/read-bible.blade.php
@@ -1,4 +1,4 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
